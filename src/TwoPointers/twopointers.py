7255787c9ca27f4af8a42bdf6b008343fd1e57a0
Arun Sisarran <arunsisarran@gmail.com>
2025-03-31 21:36:49 -0400

    .

--- src/TwoPointers/twopointers.py
+++
@@ -1,5 +0,0 @@
-# Generally two pointers refers to two indicies in an array
-# Given an array you have one pointer L on the left side
-# and another pointer R on the right side and they work
-# towards each other, performing like a squeezing motion
-# as they both make their way towards the middle of the array
