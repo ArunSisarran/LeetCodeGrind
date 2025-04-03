1cb9b6f99d2d0f25551b6fa6d3b724654a8b7001
Arun Sisarran <arunsisarran@gmail.com>
2025-04-02 20:55:08 -0400

    0560-SUBARRAY-SUM-EQUALS-K Stats: Time: 27 ms (84.95%), Space: 20.2 MB (98.96%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,28 +1,38 @@
 '''
-task : find all sub arrays that add up to target
+goal: return the amount of subarrays that can sum up to k
 
-use a hashmap to store all seen values and how many times we have seen them
-loop through the array and sum up each new element with the previous
-take the complement of the sum
-sum - k
-if that complement is in the hashmap we know we can make a subarray
-add to the counter as many times as we are able to make the target number
+observation: 
+similar to two sum?
+need to find n elements that sum up to k
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+1 + 2
+3
+
+make a hashmap
+start with 0 in hashmap if first elem is a subarray
+create a counter var to store the total
+loop through the array continually summing up each new elemnt
+take the complement sum - k
+check if the complement is in the hashmap
+if it is in hashmap increment counter as based on the amount of times its appeared 
+if not in the hashmap add the current sum as the key and the amount of times it appears as value
 return the counter
 
-hashmap is oringally populated with 0 to handle the case where a subarray is the first element
 '''
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         seen = {0:1}
-        sum = count = 0
+        counter = sum = 0
 
         for i in nums:
             sum += i
             complement = sum - k
 
             if complement in seen:
-                count += seen[complement]
-
+                counter += seen[complement]
+            
             seen[sum] = seen.get(sum, 0) + 1
-
-        return count
+        return counter
