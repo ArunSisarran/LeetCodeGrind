38d4cf153281398d813ea02dcca41b2c94306292
Arun Sisarran <arunsisarrancs@gmail.com>
2025-04-13 19:44:21 -0400

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 6 ms (55.96%), Space: 21.2 MB (61.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,30 +1,26 @@
 '''
-goal: given an integer array nums return the k most frequent elements
+goal: given a list return the k most frequent numbers in that list
 
-add all integers to a hashmap
-integer will be the key and frequency will be the value
-add all of those key value pairs to a head
-pop the heap based off of how many k we need
+add all values in the list to a hashmap with their occurances
+create a heap
+add all hashmap key values to the heap
+loop through the heap and pop the first k elements
+return those elements
 '''
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        seen = {}
+        seen = defaultdict(int)
         heap = []
+        result = []
 
-        for num in nums:
-            seen[num] = seen.get(num, 0) + 1
+        for i in nums:
+            seen[i] = seen.get(i, 0) + 1
 
-        # push the key value pairs onto the heap
-        # using -val to make it behave like a max value heap
-        # this makes it so the highest frequency would be the smallest number
-        # therefore being at the top of the heap
         for key, val in seen.items():
             heapq.heappush(heap, (-val, key))
 
-        result = []
-        # add the top k elements of the heap to the result array
         while len(result) < k:
             result.append(heapq.heappop(heap)[1])
 
