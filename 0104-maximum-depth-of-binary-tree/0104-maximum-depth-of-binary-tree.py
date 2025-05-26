4ba5ea69b176efb66f7d6d97f5b9db9f3546fd5c
Arun Sisarran <arunsisarrancs@gmail.com>
2025-05-25 22:47:13 -0400

    0104-MAXIMUM-DEPTH-OF-BINARY-TREE Stats: Time: 0 ms (100%), Space: 19 MB (48.15%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -1,23 +1,3 @@
-'''Given the root of a binary tree, return its maximum depth.
-
-A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
-
-base case:
-if not node:
-    return 0 cause it has no depth
-
-get the heights of the left and right subtrees
-recursively call the trees
-
-left = dfs(node.left)
-right = dfs(node.right)
-
-find the length of those trees from where we currently are
-
-length = max(left, right) + 1
-
-return length
-'''
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -26,15 +6,15 @@ return length
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        def dfs(node):
-            if not node:
+
+        def dfs(root):
+            if not root:
                 return 0
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
 
-            left_tree = dfs(node.left)
-            right_tree = dfs(node.right)
-
-            length = max(left_tree, right_tree) + 1
-            return length
-
+            depth = max(left, right) + 1
+            
+            return depth
         return dfs(root)
-    
