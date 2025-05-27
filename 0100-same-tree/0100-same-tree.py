03fb54a50c6e7eb62ed967f513f72893e40ce192
Arun Sisarran <arunsisarrancs@gmail.com>
2025-05-26 22:11:17 -0400

    0100-SAME-TREE Stats: Time: 0 ms (100%), Space: 17.9 MB (20.03%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,27 +1,3 @@
-'''task: check if binary tree p and the binary tree q are the same
-
-observation: for the trees to be the same the current node must be the same
-and the left and right subtrees must also be the same
-
-base cases:
-if not p and not q:
-    return True
-
-if not p or not q:
-    return False
-
-check if the subtrees and if they are the same value
-
-if (p and q) and p.val == q.val:
-    return (dfs(p.left, q.left) and dfs(p.right, q.right))
-else:
-    return False
-
-'''
-
-
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -29,15 +5,13 @@ else:
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool: 
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         def dfs(p, q):
             if not p and not q:
                 return True
-            if not p or not q:
-                return False
 
-            if (p and q) and p.val == q.val:
-                return (dfs(p.left, q.left) and dfs(p.right, q.right))
-            
+            if (p and q) and (p.val == q.val):
+                return(dfs(p.left, q.left) and dfs(p.right, q.right))
+
             return False
         return dfs(p, q)
