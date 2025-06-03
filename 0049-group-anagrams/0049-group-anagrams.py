6753321299120fbc76132136e8fdcaccae8c17cf
Arun Sisarran <arunsisarrancs@gmail.com>
2025-06-03 09:11:49 -0400

    0049-GROUP-ANAGRAMS Stats: Time: 11 ms (83.18%), Space: 20.6 MB (89.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,9 @@
-'''
-goal : given an array of strings group all the words with the same letters
-
-use a hashmap
-loop through the strings
-use the sorted string as the hashmap key
-add all strings of that same sorted key together
-return list of strings
-'''
-
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        seen = defaultdict(list)
+        map = defaultdict(list)
 
         for words in strs:
             key = "".join(sorted(words))
-            seen[key].append(words)
+            map[key].append(words)
 
-        return list(seen.values())
+        return list(map.values())
