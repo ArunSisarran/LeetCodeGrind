0fe9c302025fea08ef5087b3e2f39f8636bedb5b
Arun Sisarran <arunsisarrancs@gmail.com>
2025-06-03 09:13:54 -0400

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 3 ms (91.12%), Space: 21.2 MB (61.56%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,3 @@
-'''
-goal: given a list of nums return the k most frequent elements
-
-create a hashmap
-populate the hashmap with each value and how many time it appears
-create a heap
-create a result list
-add all key value pairs to the heap
-pop the first k elements of the heap into the result list
-return the result list
-'''
-
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         seen = defaultdict(int)
