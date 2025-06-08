cd7f5865db7ff34647db7026aaa0c003f3fa86a2
Arun Sisarran <arunsisarrancs@gmail.com>
2025-06-08 13:50:27 -0400

    0907-KOKO-EATING-BANANAS Stats: Time: 214 ms (13.14%), Space: 19.1 MB (29.54%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,5 +1,11 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def canEat(speed):
+            totalTime = 0
+            for pile in piles:
+                totalTime += math.ceil(float(pile) / speed)
+            return totalTime <= h
+
         l = 1
         r = max(piles)
         ans = 0
@@ -7,13 +13,10 @@ class Solution:
         while l <= r:
             speed = l + (r - l) // 2
 
-            totalTime = 0
-            for pile in piles:
-                totalTime += math.ceil(float(pile)/speed)
-
-            if totalTime <= h:
+            if canEat(speed):
                 ans = speed
                 r = speed - 1
             else:
                 l = speed + 1
+
         return ans
