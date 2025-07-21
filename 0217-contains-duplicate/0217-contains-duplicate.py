40239b704acd54548c6b743c05674ba84dc338e1
Arun Sisarran <arunsisarrancs@gmail.com>
2025-07-20 21:44:57 -0400

    0217-CONTAINS-DUPLICATE Stats: Time: 7 ms (93.15%), Space: 31.6 MB (28.7%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,11 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # use a set to check if a val occurs more than once
         seen = set()
-
+        # loop through the array
         for num in nums:
-            if num not in seen:
-                seen.add(num)
-            else:
+
+            # if the val appeared before return True
+            if num in seen:
                 return True
 
+            # add the current num to the set
+            seen.add(num)
+
+        # return false if no val repeated
         return False
