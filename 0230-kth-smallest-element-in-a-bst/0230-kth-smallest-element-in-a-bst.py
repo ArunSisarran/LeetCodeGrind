f4594ca29015a717a27f0210ee31e39c9306fb59
Arun Sisarran <arunsisarrancs@gmail.com>
2025-07-21 02:11:53 -0400

    0230-KTH-SMALLEST-ELEMENT-IN-A-BST Stats: Time: 3 ms (37.28%), Space: 21 MB (72.32%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,30 +1,17 @@
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        heap = []
-        seen = defaultdict(int)
-        result = []
-
         def inorder(root):
             if not root:
-                return
+                return []
 
-            inorder(root.left)
-            seen[root.val] = seen.get(root.val, 0) + 1
-            inorder(root.right)
+            return inorder(root.left) + [root.val] + inorder(root.right)
 
-        inorder(root)
+        sorted_vals = inorder(root)
 
-        for key, val in seen.items():
-            heapq.heappush(heap, (val, key))
-        while len(result) < k:
-            result.append(heapq.heappop(heap)[1])
-
-        return result[k - 1]
+        return sorted_vals[k - 1]
