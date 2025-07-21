d1d101d0e09df50bc8a7d7a5d94c44d4ae0b0c40
Arun Sisarran <arunsisarrancs@gmail.com>
2025-07-20 22:39:27 -0400

    0242-VALID-ANAGRAM Stats: Time: 15 ms (41.94%), Space: 18.5 MB (20.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,20 +7,6 @@ hashmap:
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # hashmap for the s string
-        letters_in_s = defaultdict(int)
-
-        # loop through the string and add all letters to the hashmap
-        for char in s:
-            letters_in_s[char] = letters_in_s.get(char, 0) + 1
-
-        letters_in_t = defaultdict(int)
-
-        for char in t:
-            letters_in_t[char] = letters_in_t.get(char, 0) + 1
-
-        if letters_in_s == letters_in_t:
-            return True
-        else:
-            return False
+        s, t = sorted(s), sorted(t)
+        return s == t
 
