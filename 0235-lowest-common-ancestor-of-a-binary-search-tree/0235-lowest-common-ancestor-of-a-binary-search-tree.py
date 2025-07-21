a97ae44d9e2e7a2f8a27657257b1f4e7051e64e5
Arun Sisarran <arunsisarrancs@gmail.com>
2025-07-20 22:14:32 -0400

    0235-LOWEST-COMMON-ANCESTOR-OF-A-BINARY-SEARCH-TREE Stats: Time: 65 ms (19.62%), Space: 21.1 MB (19.62%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,6 +5,12 @@
 #         self.left = None
 #         self.right = None
 
+'''
+Solution makes sense because if both values are bigger than the root then go in the right subtree,
+if both values are smaller than root then its in the left subtree. And if one is bigger and one is smaller
+then the current node is the descendent 
+'''
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         while root:
@@ -13,4 +19,7 @@ class Solution:
             elif p.val < root.val and q.val < root.val:
                 root = root.left
             else:
-                return root     
+                return root
+
+
+        
