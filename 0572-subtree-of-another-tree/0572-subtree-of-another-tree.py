7cad4fac5c4c3eb7e7976656564ff6572401759a
Arun Sisarran <arunsisarrancs@gmail.com>
2025-07-27 14:42:59 -0400

    0572-SUBTREE-OF-ANOTHER-TREE Stats: Time: 39 ms (82.79%), Space: 17.8 MB (96.8%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -4,6 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+'''
+U: We want to find out if the tree "SubRoot" exists within our main tree "Root"
+
+M: Make a helper function to determine if its the same tree
+
+P: Make a helper function to determine if its the same tree, recursively call it on the left and right subtres, then in the main function we will recursively call the helper on the left and right subtrees as well
+
+'''
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not subRoot:
@@ -11,17 +20,16 @@ class Solution:
         if not root:
             return False
 
-        if self.is_same_tree(root, subRoot):
+        if self.isSameTree(root, subRoot):
             return True
 
         return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
-
-
-    def is_same_tree(self, root, subRoot):
+        
+    def isSameTree(self, root, subRoot):
         if not root and not subRoot:
             return True
 
         if (root and subRoot) and (root.val == subRoot.val):
-            return (self.is_same_tree(root.left, subRoot.left) and self.is_same_tree(root.right, subRoot.right))
+            return (self.isSameTree(root.left, subRoot.left) and self.isSameTree(root.right, subRoot.right))
 
         return False
