1aae72529b78b81a7d9a56843298409327557b6b
Arun Sisarran <arunsisarrancs@gmail.com>
2025-07-27 14:05:28 -0400

    0226-INVERT-BINARY-TREE Stats: Time: 0 ms (100%), Space: 17.9 MB (9.41%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -4,16 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+'''
+U: Invert the binary tree meaning all nodes on the left hand side must go to the right hand side
+if possible
+
+M: I could do this by doing node.left = node.right on each root to swap its children
+
+P: Create a helper function, pass in the root, swap the roots children, and recursively call that
+helper function on the left and right subtrees so the nodes of those trees get swapped too
+'''
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def dfs(root):
+        def helper(root):
             if not root:
                 return None
 
             root.left, root.right = root.right, root.left
 
-            left = dfs(root.left)
-            right = dfs(root.right)
+            helper(root.left)
+            helper(root.right)
 
-            return root
-        return dfs(root)
+        helper(root)
+        return root
