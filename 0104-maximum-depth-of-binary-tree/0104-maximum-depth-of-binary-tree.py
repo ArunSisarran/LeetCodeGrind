5c7009eacb51695dd3aae12d605800f2e765726b
Arun Sisarran <arunsisarrancs@gmail.com>
2025-08-03 14:29:23 -0400

    0104-MAXIMUM-DEPTH-OF-BINARY-TREE Stats: Time: 2 ms (36.91%), Space: 19.1 MB (29.83%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -4,14 +4,6 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-'''
-U: Given the root of a tree, return the max depth of the tree which is the path with the most nodes connected to it 
-Ex1 : is 3 because from the root to node 7 there are 3 nodes which is the largest
-
-M: I could use a dfs algo to find the depth of the tree
-
-P: I could create a dfs helper algorithm that takes in the root as a param. I could also make a global max value to keep track of our current longest path. The dfs algo would find the depth of the left and right subtrees and we could just take the max of those two subtrees to find what the max depth is
-'''
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         def dfs(root):
