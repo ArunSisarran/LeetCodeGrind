b0d68f7417a11a3f5037265383d548d41d792bd9
Arun Sisarran <arunsisarrancs@gmail.com>
2025-08-03 13:41:30 -0400

    0100-SAME-TREE Stats: Time: 0 ms (100%), Space: 17.8 MB (39.69%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -4,14 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+'''
+U: Checking if the trees given are identical
+
+M: Use a dfs solution to recursively check each node and its value and if the values do not match return false otherwise return true
+'''
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         def dfs(p, q):
             if not p and not q:
                 return True
+            
+            if not p or not q:
+                return False
 
             if (p and q) and (p.val == q.val):
-                return(dfs(p.left, q.left) and dfs(p.right, q.right))
+                return (dfs(p.left, q.left) and dfs(p.right, q.right))
 
             return False
         return dfs(p, q)
