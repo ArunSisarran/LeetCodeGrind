0cf18421b93fb453144c44ba4d3119c1c8fcec9c
Arun Sisarran <arunsisarrancs@gmail.com>
2025-08-26 07:22:19 -0400

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 99 ms (45.18%), Space: 26.7 MB (99.29%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,28 @@
+'''
+return the maximum number you can obtain if you bought a stock on prices[i] and sold it on prices[i + n].
+
+compare adjacent values if prices[i] is greater than prices[i + 1] don't buy the stock
+if prices[i] is less than prices[i + 1] buy the stock and save the potential profit in a variable if you sold it at prices[i + 1]
+continue moving along the array, checking if we can get a larger profit, at the end return that variable we saved out max profit in
+if there was never any profit return 0
+'''
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
-        max_profit = 0
+        max_money = 0
 
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
-            else:
+        l = 0
+        r = 1
+
+        while r <= len(prices) - 1:
+            if prices[l] >= prices[r]:
                 l = r
-            
-            r += 1
+                r += 1
+            else:
+                profit = prices[r] - prices[l]
+                max_money = max(profit, max_money)
+                r += 1
 
-        return max_profit
+        return max_money
+        
