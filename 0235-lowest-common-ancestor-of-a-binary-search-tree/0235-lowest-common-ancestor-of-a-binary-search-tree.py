389824beede2df598b4bc9443e9029140e4ee5fc
Arun Sisarran <arunsisarrancs@gmail.com>
2025-08-27 20:48:47 -0400

    0235-LOWEST-COMMON-ANCESTOR-OF-A-BINARY-SEARCH-TREE Stats: Time: 48 ms (96.3%), Space: 21.2 MB (20.14%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -4,16 +4,13 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-'''
-This works because in a BST if both values are less than the root then we go left, if both values are bigger than the root then we go right. If one value is smaller and one value is bigger then the root is the lowest common ancestor
-'''
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         while root:
-            if p.val > root.val and q.val > root.val:
-                root = root.right
-            elif p.val < root.val and q.val < root.val:
+            if p.val < root.val and q.val < root.val:
                 root = root.left
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
             else:
                 return root
