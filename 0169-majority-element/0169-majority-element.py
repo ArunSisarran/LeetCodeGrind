985205d3390a48a85a9d52147c1d13af81cf33d9
Arun Sisarran <arunsisarrancs@gmail.com>
2025-09-02 13:53:46 -0400

    0169-MAJORITY-ELEMENT Stats: Time: 13 ms (18.01%), Space: 19.3 MB (50.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         seen = defaultdict(int)
-        max_num = float('-inf')
-
+        
         for num in nums:
             seen[num] = seen.get(num, 0) + 1
 
