6409b6b6981630a1fd964876aeb582728df43fdb
Arun Sisarran <arunsisarrancs@gmail.com>
2025-10-07 18:06:42 -0400

    0153-FIND-MINIMUM-IN-ROTATED-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 18.2 MB (9.5%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,30 +1,22 @@
+'''
+idea:
+do a binary search and find the middle of the array, if the middle is bigger than the right
+pointer, the array is rotated and move the left pointer to m + 1 cause we know that m can't be
+the min value, if m > r then move the r pointer to just m because we can't confidently skip
+m as the min value
+'''
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
+        l = 0 
+        r = len(nums) - 1
 
         while l < r:
             m = l + (r - l) // 2
 
-            if nums[m] >= nums[r]:
+            if nums[m] > nums[r]:
                 l = m + 1
             else:
                 r = m
 
         return nums[l]
-
-# O(logn) 
-# O(1)
-
-'''
-[3, 4, 5, 1 ,2]
- L     M     R
-
-[3, 4, 5, 1 ,2]
-          L  R
-          M
-
-[3, 4, 5, 1 ,2]
-          L
-          M
-          R          
-'''
+        
