31ee139323258308dc6989743fed7d5b08a30e96
Arun Sisarran <arunsisarrancs@gmail.com>
2025-10-07 18:07:49 -0400

    0153-FIND-MINIMUM-IN-ROTATED-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 18.1 MB (31.38%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -2,7 +2,7 @@
 idea:
 do a binary search and find the middle of the array, if the middle is bigger than the right
 pointer, the array is rotated and move the left pointer to m + 1 cause we know that m can't be
-the min value, if m > r then move the r pointer to just m because we can't confidently skip
+the min value, if m < r then move the r pointer to just m because we can't confidently skip
 m as the min value
 '''
 class Solution:
