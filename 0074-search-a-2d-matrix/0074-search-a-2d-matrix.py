787ed27e066e1c4b67047cbd6d18689b397f906d
Arun Sisarran <arunsisarrancs@gmail.com>
2025-10-07 17:35:35 -0400

    0074-SEARCH-A-2D-MATRIX Stats: Time: 0 ms (100%), Space: 17.9 MB (99.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,21 +1,42 @@
+'''
+idea:
+since each row is sorted in increasing order and the beginning of each row is always greater
+than the last element in the last row, we check the last row element to see if the target 
+would be in range of that row. Then we preform binary search on that row
+'''
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-        total = m * n
-        l = 0
-        r = total - 1
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-        while l <= r:
-            middle = (l + r) // 2
-            i = middle // n
-            j = middle % n
+        row = 0
+        col = 0
 
-            if matrix[i][j] == target:
-                return True
-            elif matrix[i][j] < target:
-                l = middle + 1
+        # while we are in bounds of the matrix
+        while row < rows:
+
+            # check if the target is within the range of the current row
+            if matrix[row][col] <= target <= matrix[row][cols - 1]:
+                l = 0
+                r = cols - 1
+
+                while l <= r:
+                    m = l + (r - l) // 2
+
+                    if matrix[row][m] == target:
+                        return True
+                    elif matrix[row][m] > target:
+                        r = m - 1
+                    else:
+                        l = m + 1
+                    
+                # if it passed the binary search and didnt return true it means the 
+                # element does not exist in the matrix so return false
+                return False
             else:
-                r = middle - 1
+                row += 1
 
         return False
+
+
+        
