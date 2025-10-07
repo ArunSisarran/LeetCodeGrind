d6c09d21854c8c6ab1aada7d0e9fe40748625d30
Arun Sisarran <arunsisarrancs@gmail.com>
2025-10-06 23:24:37 -0400

    0239-SLIDING-WINDOW-MAXIMUM Stats: Time: 138 ms (93.8%), Space: 31.1 MB (79.9%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -10,13 +10,21 @@ class Solution:
         output = []
 
         for right in range(len(nums)):
+            # monotonic decreasing queue, this makes it so the 
+            # highest element is always at the beginning of the queue
+            # while there is a q and the last element is less than
+            # the element we are putting in, pop it then add the new one
             while dq and nums[dq[-1]] < nums[right]:
                 dq.pop()
             dq.append(right)
-
+            
+            # checks to make sure if the window is still in bounds of k, if this statement
+            # is true it means that we needs to shrink our window from the left so its within k
             if left > dq[0]:
                 dq.popleft()
 
+            # checks to see if we have reached the needed size
+            # add the largest element to the output and moves the window
             if (right + 1) >= k:
                 output.append(nums[dq[0]])
                 left += 1
