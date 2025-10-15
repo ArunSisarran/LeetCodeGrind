7b88f2c97914d40c174a5bfa1615691c2f6f4dea
Arun Sisarran <arunsisarrancs@gmail.com>
2025-10-14 20:49:19 -0400

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 19 ms (53.3%), Space: 17.8 MB (82.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,42 +1,24 @@
+'''
+idea:
+use a set to check that the characters are not duplicates
+use a sliding window to help find the largest substring
+if a duplicate is found, delete all letters until that duplicate letter if found from the set
+keep a longest value variable
+'''
 class Solution:
-    '''
-    given a string return the amount of consecutive characters that
-    do not repeat
-
-    "abcabcbb" = 3 because abc
-    "bbbbb" = 1 because b
-    "pwwkew" = 3 because wke
-
-    use a set because a set cannot contain duplicates
-    keep a variable with the largest sub string seen 
-    loop through the string and if you see a char that is already
-    in the set, delete n amount of elements from the set depending on
-    where the element is
-    use the sliding window technique
-
-    why is it sliding window?
-    it is sliding window because we are looking for the longest substring of
-    something this means that we have a pointer at the beginning and then another
-    pointer that continues along the string as long as the condition is true,
-    hence the window part
-    '''
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = set()
-        max_value = 0
-        left = 0
+        chars = set()
+        max_length = 0
+        l = 0
 
-        # the right pointer is just a variable that iterates from the start
-        # of the string till the end of the string
         for right in range(len(s)):
-            # if a duplicate char is found in the set, then delete each 
-            # element in the set until the duplicate char is removed
-            # this is what we use the left pointer for, its to remove the
-            # chars starting at the beginning of the string until the duplicate is found
-            while s[right] in seen:
-                seen.remove(s[left])
-                left += 1
-            
-            seen.add(s[right])
-            max_value = max(len(seen), max_value)
+            if s[right] in chars:
+                while s[right] in chars:
+                    chars.remove(s[l])
+                    l += 1
 
-        return max_value
+            chars.add(s[right])
+            max_length = max(max_length, len(chars))
+
+        return max_length
+        
