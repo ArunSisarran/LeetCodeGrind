1f64b8738d9490c891430a22cb5ef0972cc5639b
Arun Sisarran <arunsisarrancs@gmail.com>
2025-11-05 23:20:18 -0500

    0019-REMOVE-NTH-NODE-FROM-END-OF-LIST Stats: Time: 0 ms (100%), Space: 17.7 MB (84.23%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -3,28 +3,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+loop through the linked list to find its length
+subtract length - n to see where in the list we need to remove the node
+go to the node right before it and set its next pointer to .next.next
+'''
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # create a new node and the .next points to the head of the list
-        res = ListNode(0, head)
-        # dummy node
-        dummy = res
+        length = 0
+        curr = head
 
-        # move the pointer n spaces after the beginning of the list
-        for _ in range(n):
-            head = head.next
-        
-        # while head is valid move head and dummy node, this makes it so when head 
-        # reaches the end of the list, the dummy node will be pointing to the node 
-        # right before the node that needs to be removed, this is because dummy is currently 
-        # its own node, then when head goes to the end of the list, dummy will be at n - 1
-        # nodes from the end which is the node before the node to be removed
-        while head:
-            head = head.next
-            dummy = dummy.next
-        
-        # set the node before the nth node to skip over that node
-        dummy.next = dummy.next.next
+        while curr:
+            length += 1
+            curr = curr.next
 
-        # return the head of the list
-        return res.next
+        remove = length - n
+
+        if remove == 0:
+            return head.next
+
+        curr = head
+
+        for _ in range(remove - 1):
+            curr = curr.next
+
+        curr.next = curr.next.next
+        return head
+
