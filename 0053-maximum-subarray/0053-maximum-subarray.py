70f425003e6ed5d11a7eb5973339750a0e836d1b
Arun Sisarran <arunsisarrancs@gmail.com>
2025-11-05 20:38:01 -0500

    0053-MAXIMUM-SUBARRAY Stats: Time: 52 ms (73.07%), Space: 31.7 MB (95.95%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,13 +1,21 @@
+'''
+Given an integer array nums return the sum of the max sub array
+
+create a max sum variable to keep track of the largest sum
+if the current_sum < 0, reset current_sum to 0
+keep adding to the sum and comparing with the max sum
+'''
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        biggest_sum = nums[0]
+        max_sum = nums[0]
         current_sum = 0
-
+        
         for num in nums:
+
             if current_sum < 0:
                 current_sum = 0
 
             current_sum += num
-            biggest_sum = max(current_sum, biggest_sum)
+            max_sum = max(current_sum, max_sum)
 
-        return biggest_sum
+        return max_sum
