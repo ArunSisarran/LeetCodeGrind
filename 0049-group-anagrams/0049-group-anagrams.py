f50811307ea7f4af924a5afdc53202802e034cb7
Arun Sisarran <arunsisarrancs@gmail.com>
2025-11-06 17:34:49 -0500

    0049-GROUP-ANAGRAMS Stats: Time: 13 ms (55.29%), Space: 20.6 MB (89.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,11 @@
-'''
-U: given a list of strings group all the strings with the same letters together
-
-M: we could use a hashmap to store all the letters we see
-
-P: we use the hashmap and the sorted version on the string as a key, then we keep looping through the list and sorting all the strings and adding them to the hashmap, this would make it so all strings with the same letters exactly are groupped together
-
-'''
-
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        seen = defaultdict(list)
+        
+        anagrams = defaultdict(list)
 
         for word in strs:
             sorted_word = "".join(sorted(word))
 
-            seen[sorted_word].append(word)
+            anagrams[sorted_word].append(word)
 
-        return list(seen.values())
+        return list(anagrams.values())
