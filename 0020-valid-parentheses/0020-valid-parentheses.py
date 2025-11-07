6ad15c204218856c49952e385b45a9c0ebc1d35b
Arun Sisarran <arunsisarrancs@gmail.com>
2025-11-07 17:35:19 -0500

    0020-VALID-PARENTHESES Stats: Time: 3 ms (35.43%), Space: 18 MB (15.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,30 +1,25 @@
 '''
-Create a hashmap with all pairs of parentheses
-Create a stack
-Loop through the input string 
-Push opening parentheses to the stack
-If a closing parentheses if found pop from the stack
-If a closing parenthese is found but the top of the stack is not the opening parenthese False
-If the stack is empty at the end return true else return false
+return true or false if the string contains a valid set of parentheses
+
+make a hashmap that contains closing and its respecitve opening parentheses
+make a stack
+push to the stack all the opening parentheses
+pop from the stack once it sees the closing parentheses
+if the stack is empty at the end its valid else its not
 '''
-
-
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        valid = {"}" : "{", ")" : "(", "]" : "["}
         stack = []
+        pairs = {'}':'{', ']':'[', ')':'('}
 
         for char in s:
-            if char in valid and stack:
-                if stack and stack[-1] == valid[char]:
+            if not stack:
+                stack.append(char)
+            else:
+                if char in pairs and stack[-1] == pairs[char]:
                     stack.pop()
                 else:
-                    return False
-            else:
-                stack.append(char)
+                    stack.append(char)
 
+        print(stack)
         return False if stack else True
-
-# O(n)
-# O(n)
