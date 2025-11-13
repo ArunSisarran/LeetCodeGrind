510420e4c93e17155f6868a3bc32897b8df9c0da
Arun Sisarran <arunsisarrancs@gmail.com>
2025-11-13 05:31:35 -0500

    0235-LOWEST-COMMON-ANCESTOR-OF-A-BINARY-SEARCH-TREE Stats: Time: 60 ms (46.23%), Space: 21.1 MB (47.95%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,14 +5,21 @@
 #         self.left = None
 #         self.right = None
 
+'''
+Find the lowest common ancestor in the tree and return it
+
+This is a binary search tree meaning it follows a structure that orders the 
+nodes, if any node is smaller than the current root its in the left subtree
+any node bigger in the right subtree, the lowest common ancestor would be the node
+that the p and q are not both bigger or smaller than.
+'''
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         while root:
-            if p.val < root.val and q.val < root.val:
-                root = root.left
-            elif p.val > root.val and q.val > root.val:
+            if p.val > root.val and q.val > root.val:
                 root = root.right
+            elif p.val < root.val and q.val < root.val:
+                root = root.left
             else:
                 return root
-
         
